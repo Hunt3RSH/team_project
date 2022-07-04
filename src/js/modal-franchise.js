a91d1543bfa2e7cc51fb2fc7c4394cab8371c28d
Yana-Palam <tana.palam@gmail.com>
2022-07-04 23:42:18 +0300

    add modal

--- src/js/modal-franchise.js
+++ src/js/modal-franchise.js
@@ -16,25 +16,3 @@
     refs.modalFranchise.classList.toggle('is-hidden');
   }
 })();
-
-//
-// (() => {
-//   const refs = {
-//     openFranchiseBtn: document.querySelector('[data-open-menu]'),
-//     closeFranchiseBtn: document.querySelector('[data-close-menu]'),
-//     backdropFranchise: document.querySelector('[data-backdrop]'),
-//   };
-
-//   refs.openFranchiseBtn.addEventListener('click', toggleModal);
-//   refs.closeFranchiseBtn.addEventListener('click', toggleModal);
-//   refs.backdropFranchise.addEventListener('click', logBackdropClick);
-
-//   function toggleModal() {
-//     document.body.classList.toggle('lock');
-//     refs.backdropFranchise.classList.toggle('is-hidden');
-//   }
-
-//   function logBackdropClick() {
-//     console.log('Open modal');
-//   }
-// })();
